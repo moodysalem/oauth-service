ece015a81297395acc163920458f4ecb47b15dc8
Moody Salem <moody.salem@gmail.com>
2015-09-27 19:55:10 -0500

    build for java 8, remove the redundant files

--- src/main/java/com/leaguekit/oauth/BaseApplication.java
+++ src/main/java/com/leaguekit/oauth/BaseApplication.java
@@ -5,7 +5,7 @@ import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.EncodingFilter;
 
-public class BaseApplication extends ResourceConfig {
+public class OAuthApplication extends BaseAppl{
     public BaseApplication() {
         // register the things that are typically used by a JAX-RS application
         register(JacksonFeature.class);
