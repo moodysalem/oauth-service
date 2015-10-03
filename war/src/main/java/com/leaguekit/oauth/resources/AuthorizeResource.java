9d08da7eed6e73ce28f8a3350d5f1cc3c729dd54
Moody Salem <moody.salem@gmail.com>
2015-10-03 16:03:07 -0500

    Starting to flesh out the application

--- war/src/main/java/com/leaguekit/oauth/resources/AuthorizeResource.java
+++ war/src/main/java/com/leaguekit/oauth/resources/AuthorizeResource.java
@@ -1,30 +1,51 @@
 package com.leaguekit.oauth.resources;
 
+import com.leaguekit.jaxrs.lib.exceptions.RequestProcessingException;
+import com.leaguekit.oauth.model.Application;
 import org.glassfish.jersey.server.mvc.Template;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
 
 @Path("authorize")
 public class AuthorizeResource extends BaseResource {
 
     public static class AuthorizationResponse {
+        private List<String> configurationErrors;
 
+        public List<String> getConfigurationErrors() {
+            return configurationErrors;
+        }
+        public void addConfigurationError(String error) {
+            if (configurationErrors == null) {
+                configurationErrors = new ArrayList<>();
+            }
+            configurationErrors.add(error);
+        }
     }
 
     @GET
-    @Template(name = "/templates/Login")
+    @Template(name = "/templates/Authorize")
     public AuthorizationResponse auth(
-        @HeaderParam("Referer") String referer,
         @QueryParam("response_type") String responseType,
         @QueryParam("client_id") String clientId,
         @QueryParam("redirect_uri") String redirectUri
     ) {
         AuthorizationResponse ar = new AuthorizationResponse();
 
+        if (clientId == null || redirectUri == null || responseType == null) {
+            ar.addConfigurationError("Client ID, redirect URI, and response type are all required for this endpoint.");
+        }
+
+        // first look up the Application
+
         return ar;
     }
+
+    private Application getApplication()
 
 }
--- war/src/main/java/com/leaguekit/oauth/resources/BaseResource.java
+++ war/src/main/java/com/leaguekit/oauth/resources/BaseResource.java
@@ -2,8 +2,13 @@ package com.leaguekit.oauth.resources;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
 
 public class BaseResource {
+
+    @Context
+    HttpServletRequest req;
 
     @Inject
     private EntityManager em;
