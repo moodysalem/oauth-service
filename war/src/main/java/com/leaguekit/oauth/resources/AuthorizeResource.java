76225ad7239f7a465a48bccca25af4db04a7725b
Moody Salem <moody.salem@gmail.com>
2015-10-03 16:35:10 -0500

    Some schema fixes, error page, authorize resource

--- war/src/main/java/com/leaguekit/oauth/resources/AuthorizeResource.java
+++ war/src/main/java/com/leaguekit/oauth/resources/AuthorizeResource.java
@@ -1,7 +1,6 @@
 package com.leaguekit.oauth.resources;
 
 import com.leaguekit.oauth.model.Client;
-import org.glassfish.jersey.server.mvc.Template;
 import org.glassfish.jersey.server.mvc.Viewable;
 
 import javax.persistence.criteria.CriteriaQuery;
@@ -9,40 +8,43 @@ import javax.persistence.criteria.Root;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.net.URI;
 import java.net.URL;
 import java.util.List;
 
 @Path("authorize")
 public class AuthorizeResource extends BaseResource {
 
-    public static class AuthorizationResponse {
-    }
-
-
     @GET
-    @Template(name = "/templates/Authorize")
     public Viewable auth(
         @QueryParam("response_type") String responseType,
         @QueryParam("client_id") String clientId,
         @QueryParam("redirect_uri") String redirectUri
     ) {
+        // verify all the query parameters are passed
         if (clientId == null || redirectUri == null || responseType == null) {
-            return new Viewable("/templates/Error", "Client ID, redirect URI, and response type are all required for this endpoint.");
+            return error("Client ID, redirect URI, and response type are all required for this endpoint.");
         }
-        URL toRedirect;
+        // verify redirect URL is a proper redirect URL
+        URI toRedirect;
         try {
-            toRedirect = new URL(redirectUri);
+            toRedirect = new URI(redirectUri);
         } catch (Exception e) {
-            return new Viewable("/templates/Error", "Invalid redirect URL: " + e.getMessage());
+            return error("Invalid redirect URL: " + e.getMessage());
         }
 
         // first look up the Client by the client identifier
         Client c = getClient(clientId);
         if (c == null) {
-            return new Viewable("/templates/Error", "Client ID not found.");
+            return error("Client ID not found.");
         }
-        
+
         return new Viewable("/templates/Authorize");
+    }
+
+    private Viewable error(String error) {
+        return new Viewable("/templates/Error", error);
     }
 
     private Client getClient(String clientId) {
