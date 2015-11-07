1955d9c21ea59e2c4407e64e17027daf28f16fdb
Moody Salem <moody.salem@gmail.com>
2015-11-07 14:40:40 -0600

    fix header size

--- src/main/webapp/js/views/Documentation.js
+++ src/main/webapp/js/views/Documentation.js
@@ -20,17 +20,17 @@ define([ "react", "util" ], function (React, util) {
 
       return d.div({ className: "container-fluid" }, [
         d.div({ className: "row", key: "1" }, [
-          d.div({ key: "c", className: "col-md-8" }, d.h1({
+          d.div({ key: "c", className: "col-md-8 col-lg-7 col-xl-6 col-lg-offset-1" }, d.h2({
             key: "1",
             className: "page-header"
           }, "Documentation"))
         ]),
         d.div({ className: "row", key: "2" }, [
-          d.div({ className: "col-md-4 visible-xs visible-sm", key: "toc-small" }, d.div({
+          d.div({ className: "visible-xs visible-sm", key: "toc-small" }, d.div({
             className: "well",
             key: "toc"
           }, toc)),
-          d.div({ className: "col-md-8", key: "content" }, [
+          d.div({ className: "col-md-8 col-lg-7 col-lg-offset-1", key: "content" }, [
             d.h3({ key: "intro", id: "intro" }, "Intro"),
             d.p({ key: "ip" }, "Web applications often require the user to authenticate to access their own data. These web " +
               "applications often store and transmit user data in insecure manners. Completing a secure and reliable OAuth2" +
@@ -40,7 +40,7 @@ define([ "react", "util" ], function (React, util) {
             d.p({ key: "pp" }, "To provide a stable and reliable identity platform on which you can build secure APIs, register " +
               "third party clients to use your APIs, and easily use to authenticate users in web and native applications.")
           ]),
-          d.div({ className: "col-md-4 hidden-xs hidden-sm", key: "toc-big" }, d.div({
+          d.div({ className: "col-md-4 col-lg-3 hidden-xs hidden-sm", key: "toc-big" }, d.div({
             className: "well",
             key: "toc"
           }, toc))
