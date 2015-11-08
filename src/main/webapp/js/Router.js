97da54b69ee64c98c1b6fb7137080d9a01367d2d
Moody Salem <moody.salem@gmail.com>
2015-11-07 19:40:06 -0600

    send path to google analytics

--- src/main/webapp/js/Router.js
+++ src/main/webapp/js/Router.js
@@ -1,7 +1,19 @@
 define([ "backbone", "react", "react-dom", "model", "underscore", "rbs/components/mixins/Model", "rbs/components/controls/Tappable",
-    "js/Nav", "util" ],
-  function (Backbone, React, dom, m, _, model, tp, nav, util) {
+    "js/Nav", "util", "ga" ],
+  function (Backbone, React, dom, m, _, model, tp, nav, util, ga) {
     "use strict";
+
+    var pv = _.noop, lastPath = null;
+    if (typeof ga === "function") {
+      ga("create", "UA-58623092-4", "auto");
+      pv = _.debounce(function () {
+        if (ga && window.location.pathname !== lastPath) {
+          ga("send", "pageview", {
+            page: (lastPath = window.location.pathname)
+          });
+        }
+      }, 100);
+    }
 
     // component that re-renders on application model change as well as wraps the children in a tap-friendly listener
     var wrapper = util.rf({
@@ -15,6 +27,7 @@ define([ "backbone", "react", "react-dom", "model", "underscore", "rbs/component
     var renderFile = function (file, properties) {
       require([ file ], function (comp) {
         dom.render(wrapper({ model: m }, comp(properties)), $("#app").get(0));
+        pv();
       });
     };
 
