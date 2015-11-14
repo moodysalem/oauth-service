af5ec60a4575c64244a586d670391048108d7f56
Moody Salem <moody.salem@gmail.com>
2015-11-14 11:52:13 -0600

    tests work now

--- src/test/java/com/oauth2cloud/server/oauth/OAuthTest.java
+++ src/test/java/com/oauth2cloud/server/oauth/OAuthTest.java
@@ -1,6 +1,6 @@
 package com.oauth2cloud.server.oauth;
 
-import com.leaguekit.jaxrs.lib.test.BaseTest;
+import com.moodysalem.jaxrs.lib.test.BaseTest;
 import com.oauth2cloud.server.applications.oauth.OAuthApplication;
 import org.glassfish.jersey.server.ResourceConfig;
 
