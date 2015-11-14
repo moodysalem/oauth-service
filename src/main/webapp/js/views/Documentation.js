a42dcd35822e2ce250f8379a43426b8df0d050c2
Moody Salem <moody.salem@gmail.com>
2015-11-14 12:47:38 -0600

    fix documentation and split out endpoint

--- src/main/webapp/js/views/Documentation.js
+++ src/main/webapp/js/views/Documentation.js
@@ -1,178 +1,11 @@
 /**
  *
  */
-define([ "react", "util", "rbs/components/layout/Icon", "rbs/components/layout/Tip", "underscore" ],
-  function (React, util, icon, tip, _) {
+define([ "react", "util", "underscore", "./Endpoint" ],
+  function (React, util, _, ep) {
     "use strict";
     var d = React.DOM;
     var rpt = React.PropTypes;
-
-    var ep = util.rf({
-      propTypes: {
-        method: rpt.string.isRequired,
-        endpoint: rpt.string.isRequired,
-        parameters: rpt.arrayOf(
-          rpt.shape({
-            req: rpt.bool.isRequired,
-            name: rpt.string.isRequired,
-            type: rpt.string.isRequired,
-            loc: rpt.string.isRequired,
-            desc: rpt.string.isRequired,
-            opts: rpt.arrayOf(rpt.string),
-            value: rpt.string
-          }).isRequired
-        ),
-        contentType: rpt.oneOf([ "application/x-www-form-urlencoded" ])
-      },
-
-      getDefaultProps: function () {
-        return {
-          contentType: "application/x-www-form-urlencoded"
-        };
-      },
-
-      getInitialState: function () {
-        return {
-          parameters: {}
-        };
-      },
-
-      handleChange: function (paramName, event) {
-        var val = event.target.value;
-        if (typeof val !== "string" || val.trim().length === 0) {
-          val = null;
-        }
-        var newVal = {};
-        newVal[ paramName ] = val;
-        var newParams = _.extend({}, this.state.parameters, newVal);
-        this.setState({
-          parameters: newParams
-        });
-      },
-
-      getInput: function (oneP) {
-        var val = (typeof oneP.value === "undefined" ? this.state.parameters[ oneP.name ] : oneP.value),
-          ro = typeof oneP.value !== "undefined";
-        if (_.isArray(oneP.opts)) {
-          return d.select({
-            className: "form-control",
-            readOnly: ro,
-            value: val,
-            onChange: _.bind(this.handleChange, this, oneP.name)
-          }, _.map(oneP.opts, function (o) {
-            return d.option({ key: o, value: o }, o);
-          }));
-        }
-        return d.input({
-          type: "text",
-          placeholder: oneP.name,
-          className: "form-control input-sm",
-          readOnly: (typeof oneP.value !== "undefined"),
-          value: (typeof oneP.value === "undefined" ? this.state.parameters[ oneP.name ] : oneP.value),
-          onChange: _.bind(this.handleChange, this, oneP.name)
-        });
-      },
-
-      getParameterTable: function () {
-        return d.div({ key: "d" }, [
-          d.h5({ key: "hdr" }, "Parameters"),
-          d.table({ key: "t", className: "table table-bordered table-responsive-horizontal" }, [
-            d.thead({ key: "th" },
-              d.tr({}, [
-                d.td({ key: "r" }, tip({ tip: "Whether the parameter is required as part of a valid request" }, "Required")),
-                d.td({ key: "1" }, tip({ tip: "The expected name of the parameter" }, "Name")),
-                d.td({ key: "2" }, tip({ tip: "The data type of the parameter" }, "Type")),
-                d.td({ key: "val" }, tip({ tip: "Enter a value to see an example request" }, "Value")),
-                d.td({ key: "4" }, tip({ tip: "Where the parameter should be found in the request" }, "Location")),
-                d.td({ key: "3" }, tip({ tip: "What the parameter does." }, "Description"))
-              ])
-            ),
-            d.tbody({ key: "tb" },
-              _.map(this.props.parameters, function (oneP) {
-                return d.tr({ key: oneP.name }, [
-                  d.td({ key: "r", "data-title": "Required" }, oneP.req ? "Yes" : "No"),
-                  d.td({ key: "1", "data-title": "Name" }, oneP.name),
-                  d.td({ key: "2", "data-title": "Type" }, oneP.type),
-                  d.td({ key: "val", "data-title": "Value" }, d.span({}, this.getInput(oneP))),
-                  d.td({ key: "3", "data-title": "Location" }, oneP.loc),
-                  d.td({ key: "4", "data-title": "Description" }, oneP.desc)
-                ])
-              }, this)
-            )
-          ])
-        ]);
-      },
-
-      getParamObj: function (location) {
-        location = location.toLowerCase();
-        var dataObj = {};
-        _.each(this.props.parameters, function (oneP) {
-          if ((oneP.req || oneP.value || this.state.parameters[ oneP.name ]) && oneP.loc.toLowerCase() === location) {
-            var val = typeof oneP.value !== "undefined" ? oneP.value : this.state.parameters[ oneP.name ];
-            if (typeof val !== "string") {
-              val = "";
-            }
-            dataObj[ oneP.name ] = val;
-          }
-        }, this);
-        return dataObj;
-      },
-
-      getBody: function () {
-        var dataObj = this.getParamObj("body");
-        if (_.keys(dataObj).length > 0) {
-          var toReturn = "-d '";
-          if (this.props.contentType === "application/json") {
-            toReturn += JSON.stringify(dataObj);
-          } else {
-            toReturn += $.param(dataObj, true);
-          }
-          toReturn += "'";
-          return toReturn;
-        }
-        return "";
-      },
-
-      getHeaders: function () {
-        var headerObj = this.getParamObj("header");
-        if (_.keys(headerObj).length > 0) {
-          var toReturn = [];
-          _.each(headerObj, function (value, key, list) {
-            toReturn.push("-H '" + key + ": " + value + "'");
-          });
-          return toReturn.join(" ");
-        }
-        return "";
-      },
-
-      getEndpoint: function () {
-        var paramObj = this.getParamObj("query");
-        return this.props.endpoint + (_.keys(paramObj).length > 0 ? ("?" + $.param(paramObj, true)) : "");
-      },
-
-      render: function () {
-        return d.div({ className: "card " }, [
-          d.div({ key: "ep", className: "well well-sm nowrap-scroll" }, [
-            "curl ",
-            (this.props.method !== "GET" ? d.strong({ key: "m" }, [ " -X ", this.props.method ]) : ""),
-            " ",
-            this.getEndpoint(),
-            " ",
-            this.getBody(),
-            " ",
-            this.getHeaders()
-          ]),
-          this.getParameterTable()
-        ])
-      }
-    });
-
-    var code = util.rf({
-      displayName: "code",
-      render: function () {
-        return d.span({ className: "well well-sm" }, this.props.children);
-      }
-    });
 
     return util.rf({
       displayName: "Docs",
@@ -198,7 +31,8 @@ define([ "react", "util", "rbs/components/layout/Icon", "rbs/components/layout/T
                 d.li({ key: "1" }, d.a({ href: "#authorization_code" }, "Authorization Code")),
                 d.li({ key: "2" }, d.a({ href: "#resource_owner_password" }, "Resource Owner Password")),
                 d.li({ key: "3" }, d.a({ href: "#client_credentials" }, "Client Credentials")),
-                d.li({ key: "4" }, d.a({ href: "#refresh_token" }, "Refresh Token"))
+                d.li({ key: "4" }, d.a({ href: "#refresh_token" }, "Refresh Token")),
+                d.li({ key: "5" }, d.a({ href: "#token_info" }, "Token Info"))
               ]),
               d.li({ key: "a2" }, d.a({ href: "#admin" }, "Admin"))
             ]),
@@ -429,6 +263,36 @@ define([ "react", "util", "rbs/components/layout/Icon", "rbs/components/layout/T
                       desc: "The scopes for which the access token should be valid. These scopes cannot exceed the scopes " +
                       " for which the refresh token was obtained, even if the user has accepted those scopes for the client " +
                       "to which the refresh token was distributed."
+                    }
+                  ]
+                }),
+                d.h5({ key: "tokeninfo", id: "token_info" }, "Token Info"),
+                d.p({ key: "tokeninfo_p" }, "This endpoint is used by the client and server to validate the token and retrieve basic information about the user."),
+                ep({
+                  key: "tokeninfo_ep",
+                  method: "POST",
+                  endpoint: "https://oauth2cloud.com/oauth/token/info",
+                  parameters: [
+                    {
+                      req: true,
+                      name: "token",
+                      type: "string",
+                      loc: "body",
+                      desc: "The token being queried"
+                    },
+                    {
+                      req: false,
+                      name: "client_id",
+                      type: "string",
+                      loc: "body",
+                      desc: "The ID of the client making the request. The client ID OR the application ID is required."
+                    },
+                    {
+                      req: false,
+                      name: "application_id",
+                      type: "string",
+                      loc: "body",
+                      desc: "The ID of the application making the request. The application ID OR the application ID is required."
                     }
                   ]
                 })
---
+++ src/main/webapp/js/views/Endpoint.js
@@ -0,0 +1,171 @@
+/**
+ *
+ */
+define([ "react", "util", "underscore", "rbs/components/layout/Icon", "rbs/components/layout/Tip" ],
+  function (React, util, _, icon, tip) {
+    "use strict";
+
+    var rpt = React.PropTypes;
+    var d = React.DOM;
+
+    return util.rf({
+      propTypes: {
+        method: rpt.string.isRequired,
+        endpoint: rpt.string.isRequired,
+        parameters: rpt.arrayOf(
+          rpt.shape({
+            req: rpt.bool.isRequired,
+            name: rpt.string.isRequired,
+            type: rpt.string.isRequired,
+            loc: rpt.string.isRequired,
+            desc: rpt.string.isRequired,
+            opts: rpt.arrayOf(rpt.string),
+            value: rpt.string
+          }).isRequired
+        ),
+        contentType: rpt.oneOf([ "application/x-www-form-urlencoded", "application/json" ])
+      },
+
+      getDefaultProps: function () {
+        return {
+          contentType: "application/x-www-form-urlencoded"
+        };
+      },
+
+      getInitialState: function () {
+        return {
+          parameters: {}
+        };
+      },
+
+      handleChange: function (paramName, event) {
+        var val = event.target.value;
+        if (typeof val !== "string" || val.trim().length === 0) {
+          val = null;
+        }
+        var newVal = {};
+        newVal[ paramName ] = val;
+        var newParams = _.extend({}, this.state.parameters, newVal);
+        this.setState({
+          parameters: newParams
+        });
+      },
+
+      getInput: function (oneP) {
+        var val = (typeof oneP.value === "undefined" ? this.state.parameters[ oneP.name ] : oneP.value),
+          ro = typeof oneP.value !== "undefined";
+        if (_.isArray(oneP.opts)) {
+          return d.select({
+            className: "form-control",
+            readOnly: ro,
+            value: val,
+            onChange: _.bind(this.handleChange, this, oneP.name)
+          }, _.map(oneP.opts, function (o) {
+            return d.option({ key: o, value: o }, o);
+          }));
+        }
+        return d.input({
+          type: "text",
+          placeholder: oneP.name,
+          className: "form-control input-sm",
+          readOnly: (typeof oneP.value !== "undefined"),
+          value: (typeof oneP.value === "undefined" ? this.state.parameters[ oneP.name ] : oneP.value),
+          onChange: _.bind(this.handleChange, this, oneP.name)
+        });
+      },
+
+      getParameterTable: function () {
+        return d.div({ key: "d" }, [
+          d.h5({ key: "hdr" }, "Parameters"),
+          d.table({ key: "t", className: "table table-bordered table-responsive-horizontal" }, [
+            d.thead({ key: "th" },
+              d.tr({}, [
+                d.td({ key: "r" }, tip({ tip: "Whether the parameter is required as part of a valid request" }, "Required")),
+                d.td({ key: "1" }, tip({ tip: "The expected name of the parameter" }, "Name")),
+                d.td({ key: "2" }, tip({ tip: "The data type of the parameter" }, "Type")),
+                d.td({ key: "val" }, tip({ tip: "Enter a value to see an example request" }, "Value")),
+                d.td({ key: "4" }, tip({ tip: "Where the parameter should be found in the request" }, "Location")),
+                d.td({ key: "3" }, tip({ tip: "What the parameter does." }, "Description"))
+              ])
+            ),
+            d.tbody({ key: "tb" },
+              _.map(this.props.parameters, function (oneP) {
+                return d.tr({ key: oneP.name }, [
+                  d.td({ key: "r", "data-title": "Required" }, oneP.req ? "Yes" : "No"),
+                  d.td({ key: "1", "data-title": "Name" }, oneP.name),
+                  d.td({ key: "2", "data-title": "Type" }, oneP.type),
+                  d.td({ key: "val", "data-title": "Value" }, d.span({}, this.getInput(oneP))),
+                  d.td({ key: "3", "data-title": "Location" }, oneP.loc),
+                  d.td({ key: "4", "data-title": "Description" }, oneP.desc)
+                ])
+              }, this)
+            )
+          ])
+        ]);
+      },
+
+      getParamObj: function (location) {
+        location = location.toLowerCase();
+        var dataObj = {};
+        _.each(this.props.parameters, function (oneP) {
+          if ((oneP.req || oneP.value || this.state.parameters[ oneP.name ]) && oneP.loc.toLowerCase() === location) {
+            var val = typeof oneP.value !== "undefined" ? oneP.value : this.state.parameters[ oneP.name ];
+            if (typeof val !== "string") {
+              val = "";
+            }
+            dataObj[ oneP.name ] = val;
+          }
+        }, this);
+        return dataObj;
+      },
+
+      getBody: function () {
+        var dataObj = this.getParamObj("body");
+        if (_.keys(dataObj).length > 0) {
+          var toReturn = "-d '";
+          if (this.props.contentType === "application/json") {
+            toReturn += JSON.stringify(dataObj);
+          } else {
+            toReturn += $.param(dataObj, true);
+          }
+          toReturn += "'";
+          return toReturn;
+        }
+        return "";
+      },
+
+      getHeaders: function () {
+        var headerObj = this.getParamObj("header");
+        if (_.keys(headerObj).length > 0) {
+          var toReturn = [];
+          _.each(headerObj, function (value, key, list) {
+            toReturn.push("-H '" + key + ": " + value + "'");
+          });
+          return toReturn.join(" ");
+        }
+        return "";
+      },
+
+      getEndpoint: function () {
+        var paramObj = this.getParamObj("query");
+        return this.props.endpoint + (_.keys(paramObj).length > 0 ? ("?" + $.param(paramObj, true)) : "");
+      },
+
+      render: function () {
+        return d.div({ className: "card " }, [
+          d.div({ key: "ep", className: "well well-sm nowrap-scroll" }, [
+            "curl",
+            " ",
+            (this.props.method !== "GET" ? d.strong({ key: "m" }, [ " -X ", this.props.method ]) : ""),
+            " ",
+            this.getEndpoint(),
+            " ",
+            this.getBody(),
+            " ",
+            this.getHeaders()
+          ]),
+          this.getParameterTable()
+        ])
+      }
+    });
+  });
