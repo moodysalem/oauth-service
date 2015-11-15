e408f3bea0855d8678e42929ee6b2c122aeb4dc3
Moody Salem <moody.salem@gmail.com>
2015-11-15 03:58:47 -0600

    debug information

--- src/main/resources/templates/Head.ftl
+++ src/main/resources/templates/Head.ftl
@@ -1,7 +1,7 @@
 <#setting url_escaping_charset='UTF-8'>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 <meta name="description" content="Log In Page">
 <meta name="author" content="Moody Salem">
 
--- src/main/webapp/js/App.js
+++ src/main/webapp/js/App.js
@@ -12,73 +12,83 @@ window.define([ "rbs/RequireConfig" ], function (rc) {
   window.require.config(rc);
 
   txt("Loading dependencies...");
-  window.require([ "js/OAuth2", "backbone", "jquery", "promise-polyfill", "util" ], function (oauth2, Backbone, $, pp, util) {
-    var m = new Backbone.Model();
-    window.define("model", m);
+  window.require([ "js/OAuth2", "backbone", "jquery", "promise-polyfill", "util" ],
+    function (oauth2, Backbone, $, pp, util) {
+      util.debug("defining model...");
+      var m = new Backbone.Model();
+      window.define("model", m);
 
-    var start = function () {
-      txt("Initializing application...");
-      require([ "js/Router" ], function (router) {
-        var r = new router();
-        window.define("router", r);
+      util.debug("defining start function..");
+      var start = function () {
+        util.debug("start function called...");
+        txt("Initializing application...");
+        require([ "js/Router" ], function (router) {
+          var r = new router();
+          window.define("router", r);
 
-        $(function () {
-          $(document).on("click", "[href]", function (e) {
-            var href = $(e.target).closest("[href]").attr("href");
-            if (typeof href !== "string") {
-              return;
-            }
-            if (href.length > 0 && href[ 0 ] === "#") {
-              e.preventDefault();
-              if (href.length > 1) {
-                var els = $(href);
-                if (els.length == 1) {
-                  var st = els.offset().top;
-                  if (st > 0) {
-                    $("html, body").animate({ scrollTop: st + "px" });
+          util.debug("router loaded...");
+          $(function () {
+            $(document).on("click", "[href]", function (e) {
+              var href = $(e.target).closest("[href]").attr("href");
+              if (typeof href !== "string") {
+                return;
+              }
+              if (href.length > 0 && href[ 0 ] === "#") {
+                e.preventDefault();
+                if (href.length > 1) {
+                  var els = $(href);
+                  if (els.length == 1) {
+                    var st = els.offset().top;
+                    if (st > 0) {
+                      $("html, body").animate({ scrollTop: st + "px" });
+                    }
                   }
                 }
+                return;
               }
-              return;
-            }
-            var isInternal = util.internalLink(href);
-            if (isInternal) {
-              e.preventDefault();
-            }
-            if (isInternal) {
-              r.navigate(href, { trigger: true });
-            }
+              var isInternal = util.internalLink(href);
+              if (isInternal) {
+                e.preventDefault();
+              }
+              if (isInternal) {
+                r.navigate(href, { trigger: true });
+              }
+            });
           });
+
+          util.debug("starting router..");
+          Backbone.history.start({ pushState: true });
         });
-
-        Backbone.history.start({ pushState: true });
-      });
-    };
+      };
 
 
-    //configure ajax calls to the API to send our token in the authorization header whenever we are logged in
-    $(document).ajaxSend(function (event, jqXhr, ajaxOptions) {
-      var url = ajaxOptions.url;
-      if (url.indexOf("api") === 0) {
-        if (m.has("token.access_token")) {
-          jqXhr.setRequestHeader("Authorization", "bearer " + m.get("token.access_token"));
+      //configure ajax calls to the API to send our token in the authorization header whenever we are logged in
+      $(document).ajaxSend(function (event, jqXhr, ajaxOptions) {
+        var url = ajaxOptions.url;
+        if (url.indexOf("api") === 0) {
+          if (m.has("token.access_token")) {
+            jqXhr.setRequestHeader("Authorization", "bearer " + m.get("token.access_token"));
+          }
         }
-      }
-    });
+      });
 
-    oauth2.init({
-      clientId: window.clientId,
-      token: window.hashObject && window.hashObject.access_token,
-      authorizeUrl: "/oauth/authorize",
-      tokenInfoUrl: "/oauth/token/info"
-    });
+      util.debug("initializing oauth2");
+      oauth2.init({
+        clientId: window.clientId,
+        token: window.hashObject && window.hashObject.access_token,
+        authorizeUrl: "/oauth/authorize",
+        tokenInfoUrl: "/oauth/token/info"
+      });
 
-    oauth2.getLoginStatus().then(function (token) {
-      m.set("token", token);
-      start();
-    }, function () {
-      start();
-    });
+      util.debug("getting login status");
+      oauth2.getLoginStatus().then(function (token) {
+        util.debug("logged in");
+        m.set("token", token);
+        start();
+      }, function () {
+        util.debug("not logged in");
+        start();
+      });
 
-  });
+    });
 });
