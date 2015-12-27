f1941588455e3b1ed479d61b6a9594371b13aab4
Moody Salem <moody.salem@gmail.com>
2015-12-27 14:15:52 -0600

    more tests

--- src/test/java/com/oauth2cloud/server/admin/ApplicationsTest.java
+++ src/test/java/com/oauth2cloud/server/admin/ApplicationsTest.java
@@ -1,9 +1,12 @@
 package com.oauth2cloud.server.admin;
 
 import com.oauth2cloud.server.OAuth2CloudTest;
+import com.oauth2cloud.server.hibernate.model.Application;
 import com.oauth2cloud.server.hibernate.model.TokenResponse;
 import com.oauth2cloud.server.rest.OAuth2Cloud;
 import org.testng.annotations.Test;
+
+import javax.ws.rs.client.Entity;
 
 public class ApplicationsTest extends OAuth2CloudTest {
 
@@ -13,8 +16,45 @@ public class ApplicationsTest extends OAuth2CloudTest {
 
         assert target(OAuth2Cloud.API).path("applications")
             .request()
-            .header(AUTH_HEADER, "bearer " +  tr.getAccessToken())
+            .header(AUTH_HEADER, "bearer " + tr.getAccessToken())
             .get().getStatus() == 200;
+    }
+
+    @Test
+    public void testCRUD() {
+        TokenResponse tr = getToken();
+        String faviconUrl = "https://google.com/test.png";
+        Application app = new Application();
+        app.setName("Test App");
+        app.setSupportEmail("moody.salem@gmail.com");
+
+        app = target(OAuth2Cloud.API).path("applications")
+            .request()
+            .header(AUTH_HEADER, "bearer " + tr.getAccessToken())
+            .post(Entity.json(app), Application.class);
+
+        assert app.getId() != 0;
+        assert app.getName().equals("Test App");
+        assert app.getFaviconUrl() == null;
+
+        // test edit
+        app.setFaviconUrl(faviconUrl);
+        app = target(OAuth2Cloud.API).path("applications").path(Long.toString(app.getId()))
+            .request()
+            .header(AUTH_HEADER, "bearer " + tr.getAccessToken())
+            .put(Entity.json(app), Application.class);
+
+        assert app.getFaviconUrl().equals(faviconUrl);
+
+        assert target(OAuth2Cloud.API).path("applications").path(Long.toString(app.getId()))
+            .request()
+            .header(AUTH_HEADER, "bearer " + tr.getAccessToken())
+            .delete().getStatus() == 204;
+
+        assert target(OAuth2Cloud.API).path("applications").path(Long.toString(app.getId()))
+            .request()
+            .header(AUTH_HEADER, "bearer " + tr.getAccessToken())
+            .get().getStatus() == 404;
     }
 
 }
--- src/test/java/com/oauth2cloud/server/oauth/AuthorizeTest.java
+++ src/test/java/com/oauth2cloud/server/oauth/AuthorizeTest.java
@@ -1,33 +1,80 @@
 package com.oauth2cloud.server.oauth;
 
 import com.oauth2cloud.server.OAuth2CloudTest;
+import com.oauth2cloud.server.rest.OAuth2Cloud;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.Response;
 
-import static org.testng.Assert.assertTrue;
-
 
 public class AuthorizeTest extends OAuth2CloudTest {
+
+    @Test
+    public void testBadRequests() {
+        // code is not allowed for this client
+        assert target(OAuth2Cloud.OAUTH).path("authorize")
+            .queryParam("client_id", CLIENT_ID)
+            .queryParam("redirect_uri", "http://localhost:8080")
+            .queryParam("response_type", "code")
+            .request()
+            .get().getStatus() == 400;
+
+        // invalid redirect URI
+        assert target(OAuth2Cloud.OAUTH).path("authorize")
+            .queryParam("client_id", CLIENT_ID)
+            .queryParam("redirect_uri", "http://localhost:8081")
+            .queryParam("response_type", "token")
+            .request()
+            .get().getStatus() == 400;
+
+
+        // invalid client id
+        assert target(OAuth2Cloud.OAUTH).path("authorize")
+            .queryParam("client_id", CLIENT_ID + "1")
+            .queryParam("redirect_uri", "http://localhost:8080")
+            .queryParam("response_type", "token")
+            .request()
+            .get().getStatus() == 400;
+    }
 
 
     @Test
     public void testAuthorizeEndpoint() {
-        Response r1 = target("oauth/authorize")
-                .queryParam("client_id", CLIENT_ID)
-                .queryParam("redirect_uri", "http://localhost:8080")
-                .queryParam("response_type", "code")
-                .request()
-                .get();
+        Response auth = target(OAuth2Cloud.OAUTH).path("authorize")
+            .queryParam("client_id", CLIENT_ID)
+            .queryParam("redirect_uri", "http://localhost:8080")
+            .queryParam("response_type", "token")
+            .request()
+            .get();
 
-        Response r2 = target("oauth/authorize")
-                .queryParam("client_id", CLIENT_ID)
-                .queryParam("redirect_uri", "http://localhost:8080")
-                .queryParam("response_type", "token")
-                .request()
-                .get();
+        assert auth.getStatus() == 200;
 
-        assertTrue(r1.getStatus() == 400 && r2.getStatus() == 200);
+        // parse the dom of the successful request
+        Document doc = Jsoup.parse(auth.readEntity(String.class));
+
+        Element head = doc.select("head").first();
+        String fi = head.select("link[rel=\"icon\"]").first().attr("href");
+        assert "https://s3.amazonaws.com/oauth2cloud-static-assets/favicon-chain.ico".equals(fi);
+
+        Elements ss = head.select("link[rel=\"stylesheet\"]");
+        // bootstrap stylesheet
+        assert "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css"
+            .equals(ss.first().attr("href"));
+        // custom stylesheet
+        assert "https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/cosmo/bootstrap.min.css"
+            .equals(ss.get(1).attr("href"));
+        // fontawesome
+        assert "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css"
+            .equals(ss.get(2).attr("href"));
+
+        // page title
+        assert "OAuth2 Cloud Log In"
+            .equals(head.select("title").text());
+
     }
 
 }
