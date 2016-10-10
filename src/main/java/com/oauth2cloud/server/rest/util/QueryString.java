8447db9a59bff6e1c46d55e1f7da0f7c36ac339c
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2016-10-09 23:51:32 -0500

    Clean up the query string class

--- src/main/java/com/oauth2cloud/server/rest/util/QueryString.java
+++ src/main/java/com/oauth2cloud/server/rest/util/QueryString.java
@@ -8,6 +8,10 @@ import java.util.logging.Logger;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public abstract class QueryString {
+    private static final String UTF_8 = "UTF-8";
+    private static final Logger LOG = Logger.getLogger(QueryString.class.getName());
+    private static final char AMPERSAND = '&', EQUALS = '=';
+
     /**
      * Helper function that converts a map to its query string representation. This is used when setting the fragment
      * in the response uri of a token grant flow
@@ -22,12 +26,12 @@ public abstract class QueryString {
             for (final String key : map.keySet()) {
                 for (final String value : map.get(key)) {
                     if (!isBlank(value) && sb.length() > 0) {
-                        sb.append('&');
+                        sb.append(AMPERSAND);
                     }
                     try {
-                        sb.append(URLEncoder.encode(key, "UTF-8")).append('=').append(URLEncoder.encode(value, "UTF-8"));
+                        sb.append(URLEncoder.encode(key, UTF_8)).append(EQUALS).append(URLEncoder.encode(value, UTF_8));
                     } catch (Exception e) {
-                        Logger.getAnonymousLogger().log(Level.SEVERE, "Failed to encode map", e);
+                        LOG.log(Level.SEVERE, "Failed to encode map", e);
                     }
                 }
             }
