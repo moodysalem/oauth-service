fc271485a4febf292a2f757495905b374907af66
moodysalem <a454465c8beef70811a01489042c79b7f235df5e>
2016-10-29 11:18:01 -0500

    improve query string test

--- src/test/java/com/oauth2cloud/server/rest/util/QueryStringTest.java
+++ src/test/java/com/oauth2cloud/server/rest/util/QueryStringTest.java
@@ -4,15 +4,28 @@ import org.testng.annotations.Test;
 
 import javax.ws.rs.core.MultivaluedHashMap;
 import javax.ws.rs.core.MultivaluedMap;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
 public class QueryStringTest {
 
     @Test
-    public void testQueryString() {
-        final MultivaluedMap<String,String> testMap = new MultivaluedHashMap<>();
+    public void testQueryString() throws UnsupportedEncodingException {
+        final MultivaluedMap<String, String> testMap = new MultivaluedHashMap<>();
 
         assertEquals(QueryString.mapToQueryString(testMap), "");
+
+        testMap.putSingle("scope", "");
+        assertEquals(QueryString.mapToQueryString(testMap), "scope=");
+
+        testMap.put("scope", Arrays.asList("red", "", "blue"));
+        assertEquals(QueryString.mapToQueryString(testMap), "scope=red&scope=&scope=blue");
+
+        testMap.putSingle("hello", "blue car down");
+        assertEquals(URLDecoder.decode(QueryString.mapToQueryString(testMap), "UTF-8"),
+                "scope=red&scope=&scope=blue&hello=blue car down");
     }
 }
